09d1c74db57d6de67e0728ea0a4745edf7ee3895
Ebenézer Teles <ebteles@gmail.com>
2023-03-12 16:30:41 -0300

    adicionando suporte ao mkdocstrings

--- README.md
+++ README.md
@@ -8,6 +8,9 @@
 poetry new [nome_do_projeto]
 poetry config virtualenvs.in-project true
 poetry shell
+
+## outros comandos
+poetry list
 ```
 
 **2. Adicionar pytest e Lint**
@@ -37,3 +40,58 @@ mkdocs new .  # pasta "docs" será criada
 ```
 
 **5. Configuraçõeos do Mkdocs em "mkdocs..yml"**
+
+```yml
+site_name: Notas
+repo_url: https://github.com/ebteles/notas
+repo_name: ebteles/notas
+edit_uri: tree/master/docs
+
+theme:
+  name: material
+  language: pt-BR
+  logo: assets/notas_001.png
+  favicon: assets/notas_001.png
+
+markdown_extensions:
+  - attr_list
+
+extra_css:
+  - stylesheets/extra.css
+
+plugins:
+  - mkdocstrings:
+      handlers:
+        python:
+          paths: [notas]
+```
+
+**6. Configuraçõeos do PyTest**
+
+Configurações realizadas diretamente dentro do **pyproject.toml**.
+
+```bash
+[tool.pytest.ini_options]
+pythonpath = "."
+addopts = "--doctest-modules"
+```
+
+**7. Taskpy**
+
+Simplicar linhas de comando no terminal.
+Configurações realizadas diretamente dentro do **pyproject.toml**.
+
+```bash
+# instalação:
+poetry add --group dev taskipy
+
+# execução
+task [nome_da_task]
+exemplo: task test
+
+# configuração
+[tool.taskipy.tasks]
+docs = "mkdocs serve"
+test = "pytest -s -x --cov=notas -vv"
+post_test = "coverage html
+```
---
+++ notas/escalas.py
@@ -0,0 +1,42 @@
+def escalas(tonica: str, tonalidade):
+    """
+    Gera uma escala apartir de uma tônica e uma tonalidade.
+
+    Args:
+        tonica: Nota que será a tônica da escala
+        tonalidade: Tonialidade da escala
+
+    Returns:
+        Um dicionário com as notas da escala e os graus.
+
+    Raises:
+        ValueError: Caso a tônica não seja uma nota valida.
+        KeyError: Caso a escala não exista ou não tenha sido implementada.
+
+    Examples:
+        >>> escala('C', 'maior')
+        {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
+        >>> escala('a', 'menor')
+        {'notas': ['A', 'B', 'C', 'D', 'E', 'F', 'G'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
+    """
+
+    tonica = tonica.upper()
+    try:
+        intervalos = ESCALAS[tonalidade]
+        tonica_pos = NOTAS.index(tonica)
+    except ValueError:
+        raise ValueError(f'Essa nota não existe, tente uma dessas {NOTAS}')
+    except KeyError:
+        raise KeyError(
+            'Essa escala não existe ou não foi implementada. '
+            f'Tente uma dessas {list(ESCALAS.keys())}'
+        )
+
+    temp = []
+
+    for intervalo in intervalos:
+        nota = (tonica_pos + intervalo) % 12
+        temp.append(NOTAS[nota])
+
+    return {'notas': temp, 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
+    
